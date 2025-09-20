8f88fb893426f65159d8c1794a51b57d1968e4ee
elmehdiaitami <elmehdiaitami@gmail.com>
2025-09-20 15:13:21 +0100

    app tested localy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Simple Flask application'
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
